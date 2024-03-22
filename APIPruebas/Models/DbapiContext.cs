328b8f21328d64cdb89ada85ad5153dcb9a02651
DanielCuyuch07 <danielproyectos13@gmail.com>
2024-03-21 18:19:51 -0600

    [UPDATE] Se modifico el archivo appseting, program.cs para la conexion con la base de datos y se instalaron las dependencias

---
+++ APIPruebas/Models/DbapiContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIPruebas.Models;
+
+public partial class DbapiContext : DbContext
+{
+    public DbapiContext()
+    {
+    }
+
+    public DbapiContext(DbContextOptions<DbapiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categorium> Categoria { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local); DataBase=DBAPI; TrustServerCertificate=true; Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categorium>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__CATEGORI__A3C02A10BA11B944");
+
+            entity.ToTable("CATEGORIA");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__PRODUCTO__09889210A8D17130");
+
+            entity.ToTable("PRODUCTO");
+
+            entity.Property(e => e.CodigoBarra)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Marca)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("FK_IDCATEGORIA");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
